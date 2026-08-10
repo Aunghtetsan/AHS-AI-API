5c16af426a410d3f731738a7c8b17bfae863a27b
Aunghtetsan <zawtika525@gmail.com>
2026-08-10 18:04:43 +0630

    Initial calculator code

--- calculator.py
+++ calculator.py
@@ -1,1 +1,14 @@
-print('Hello World')
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return 'Error: Division by zero'
+    else:
+        return a / b
